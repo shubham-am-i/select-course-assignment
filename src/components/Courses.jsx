de3ae47ac1605c7fb089a3551773afb076158670
shubhamLLT <shubham@linkedlist.tech>
2023-02-24 17:17:39 +0530

    complete new course loading functionality on refresh of page

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,24 +1,15 @@
 import {useEffect, useLayoutEffect, useState} from 'react'
 
 export default function Container() {
-  const [data, setData] = useState({})
   const [course, setCourse] = useState([])
 
   useLayoutEffect(() => {
+    const randomNumber = Math.floor(Math.random() * 3)
     ;(async function fetchData() {
       const response = await fetch('src/lib/data.json')
       const courses = await response.json()
-      setData(courses)
+      setCourse(courses[randomNumber])
     })()
-  }, [])
-
-  useEffect(() => {
-    if (Object.keys(data).length > 0) {
-      const keys = Object.keys(data)
-      const randomIndex = Math.floor(Math.random() * keys.length)
-      const randomKey = keys[randomIndex]
-      setCourse(data[randomKey])
-    }
   }, [])
 
   console.log(course)
