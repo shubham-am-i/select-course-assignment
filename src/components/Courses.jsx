f47c383ef7afb49704fd7a878756ff029c0271f4
shubhamLLT <shubham@linkedlist.tech>
2023-02-24 16:51:25 +0530

    save random course in state on initial render - partial complete

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,9 +1,10 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useLayoutEffect, useState} from 'react'
 
 export default function Container() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState({})
+  const [course, setCourse] = useState([])
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     ;(async function fetchData() {
       const response = await fetch('src/lib/data.json')
       const courses = await response.json()
@@ -11,7 +12,16 @@ export default function Container() {
     })()
   }, [])
 
-  console.log(data)
+  useEffect(() => {
+    if (Object.keys(data).length > 0) {
+      const keys = Object.keys(data)
+      const randomIndex = Math.floor(Math.random() * keys.length)
+      const randomKey = keys[randomIndex]
+      setCourse(data[randomKey])
+    }
+  }, [])
+
+  console.log(course)
 
   return <div></div>
 }
